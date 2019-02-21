545ba1f4d97d6ab48c478e6711e6a0c29357fe91
Rob Hess <robwhess@gmail.com>
2019-02-21 13:21:45 -0800

    Add RecyclerView machinery to SavedReposActivity.

--- app/src/main/java/com/example/android/githubsearch/SavedReposActivity.java
+++ app/src/main/java/com/example/android/githubsearch/SavedReposActivity.java
@@ -1,13 +1,33 @@
 package com.example.android.githubsearch;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
-public class SavedReposActivity extends AppCompatActivity {
+import com.example.android.githubsearch.data.GitHubRepo;
+import com.example.android.githubsearch.utils.GitHubUtils;
+
+public class SavedReposActivity extends AppCompatActivity implements GitHubSearchAdapter.OnSearchItemClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saved_search_results);
+
+        RecyclerView savedReposRV = findViewById(R.id.rv_saved_repos);
+        savedReposRV.setLayoutManager(new LinearLayoutManager(this));
+        savedReposRV.setHasFixedSize(true);
+
+        GitHubSearchAdapter adapter = new GitHubSearchAdapter(this);
+        savedReposRV.setAdapter(adapter);
+    }
+
+    @Override
+    public void onSearchItemClick(GitHubRepo repo) {
+        Intent intent = new Intent(this, RepoDetailActivity.class);
+        intent.putExtra(GitHubUtils.EXTRA_GITHUB_REPO, repo);
+        startActivity(intent);
     }
 }
